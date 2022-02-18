bcd135d1fa17e5512a2a0f454d12ab2ae8635951
Sujoy Mistry <sm2253@cse.jgec.ac.in>
2022-02-18 21:55:17 +0530

    Updates

---
+++ precache-manifest.eec15255e768802f91cf30aa23385a89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2abc6c0e693e7ce6ed151a25c771c96b",
+    "url": "/Text_Converter/index.html"
+  },
+  {
+    "revision": "08901a592d323db1ecb1",
+    "url": "/Text_Converter/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "60b19bef5f28599cc29a",
+    "url": "/Text_Converter/static/js/2.a84fbd71.chunk.js"
+  },
+  {
+    "revision": "a03a2b79c7dfa4b66d3d",
+    "url": "/Text_Converter/static/js/3.7fa365a8.chunk.js"
+  },
+  {
+    "revision": "08901a592d323db1ecb1",
+    "url": "/Text_Converter/static/js/main.0d0f5138.chunk.js"
+  },
+  {
+    "revision": "341efbdea1027b18f871",
+    "url": "/Text_Converter/static/js/runtime~main.5026f09e.js"
+  }
+]);
